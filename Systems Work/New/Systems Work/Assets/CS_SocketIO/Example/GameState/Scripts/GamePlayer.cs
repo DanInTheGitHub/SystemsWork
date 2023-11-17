4c549ce8ba2c05d8ba93876d24f44215e3ffa6ef
Dan <109627357+DanInTheGitHub@users.noreply.github.com>
2023-11-17 04:23:04 -0500

    Update

--- Systems Work/New/Systems Work/Assets/CS_SocketIO/Example/GameState/Scripts/GamePlayer.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GamePlayer : MonoBehaviour
-{
-    public string Id { get; set; }
-    public string Username { get; set; }
-}
